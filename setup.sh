15e80c380fef14a1c7bd316a4bf6aa0ba966477a
poguepotamus <matthewpogue606@gmail.com>
2019-12-28 17:52:09 -0600

    Preventing the setup file from writing our message many times.

--- setup.sh
+++ setup.sh
@@ -8,11 +8,15 @@ fi
 
 # Collecting information that we plan to write to the file
 runCommandsFile="$HOME/.bashrc"
+# This is just a method of storing the following text, save the leading whitespace, to a variable. The following lines (before EOM), are being stored in a variable called `VAR`.
 read -r -d '' VAR <<- EOM
 	# Custom bash file's main file
 	. '$HOME/.eaftos/bashrc'
 EOM
 
-# Writing to rc file
-echo "" >> $runCommandsFile
-echo "$VAR" >> $runCommandsFile
+# Writing to rc file if commands aren't there
+grep "$VAR" $runCommandsFile >> /dev/null
+if [[ $? != 0 ]]; then # Our message is not in the file, as grep did not find it.
+  echo "" >> $runCommandsFile
+  echo "$VAR" >> $runCommandsFile
+fi
