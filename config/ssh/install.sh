c4c18fc5ac5767f5dafb4267a2994fa1c925ea62
Matthew Pogue <matthewpogue606@gmail.com>
2020-12-15 10:18:38 -0600

    Removing unessessary file copy

--- config/ssh/install.sh
+++ config/ssh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp $CONFIG_PATH/config/ssh/authorized_keys $HOME/.ssh/
-cp $CONFIG_PATH/config/ssh/config          $HOME/.ssh/
+#cp $CONFIG_PATH/config/ssh/config          $HOME/.ssh/
 
 # sshd_config is at:
 # w10: /c/ProgramData/ssh/sshd_config
