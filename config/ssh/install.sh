0db2bd106a195c3deb45f5e28d9513f58028699b
Matthew Pogue <matthewpogue606@gmail.com>
2021-02-10 12:08:58 -0600

    Adding an ssh config for new athena server

--- config/ssh/install.sh
+++ config/ssh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp $EAFTOS_PATH/config/ssh/authorized_keys $HOME/.ssh/
-#cp $EAFTOS_PATH/config/ssh/config          $HOME/.ssh/
+cp $EAFTOS_PATH/config/ssh/config          $HOME/.ssh/
 
 # sshd_config is at:
 # w10: /c/ProgramData/ssh/sshd_config
